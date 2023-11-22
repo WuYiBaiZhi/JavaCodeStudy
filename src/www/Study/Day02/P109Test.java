487bd45761731c4e5f03766f3d5724992968a75c
无意白纸 <WuYiBaiZhi@163.com>
2023-11-23 01:00:31 +0800

    使用两种方法操作字符串倒转

--- src/www/Study/Day02/P109Test.java
+++ src/www/Study/Day02/P109Test.java
@@ -3,19 +3,26 @@ package www.Study.Day02;
 public class P109Test {
     // 反转字符串
     public static void main(String[] args) {
-        System.out.println(rotate("123"));
+        System.out.println(rotate("12345"));
+        System.out.println(rotateToListToStr("12345"));
     }
+
     // 使用字符串截取实现
     public static String rotate(String string) {
         StringBuilder stringBuilder = new StringBuilder(string);
-        stringBuilder.append(string.charAt(0));
-        string = stringBuilder.substring(1);
-        return  string;
+        for (int length = string.length() - 1; length >= 0; length--)
+            stringBuilder.append(string.charAt(length));
+        string = stringBuilder.substring(string.length());
+        return string;
     }
+
+    // 使用列表实现
     public static String rotateToListToStr(String string) {
-        StringBuilder stringBuilder = new StringBuilder(string);
-        stringBuilder.append(string.charAt(0));
-        string = stringBuilder.substring(1);
-        return  string;
+        char[] chars = new char[string.length()];
+        String newString = "";
+        for (int length = string.length() - 1; length >= 0; length--) {
+            newString += string.charAt(length);
+        }
+        return newString;
     }
 }
