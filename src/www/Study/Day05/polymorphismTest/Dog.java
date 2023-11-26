36c1db937816abcc2a06962184f63af2976e2f48
无意白纸 <WuYiBaiZhi@163.com>
2023-11-26 20:09:23 +0800

    添加构造函数

--- src/www/Study/Day05/polymorphismTest/Dog.java
+++ src/www/Study/Day05/polymorphismTest/Dog.java
@@ -13,6 +13,6 @@ public class Dog extends Animal{
         System.out.printf("%d岁的%s的狗正在死死的抱住%s猛吃\n",getAge(),getColor(),something);
     }
     public void lookHome() {
-        System.out.println("狗狗在看家");
+        System.out.println("小狗在看家");
     }
 }
