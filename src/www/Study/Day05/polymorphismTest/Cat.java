51eaa00a45ecd050549badedeccd0215690cdd8d
无意白纸 <WuYiBaiZhi@163.com>
2023-11-26 18:28:21 +0800

    添加函数构造

--- src/www/Study/Day05/polymorphismTest/Cat.java
+++ src/www/Study/Day05/polymorphismTest/Cat.java
@@ -1,6 +1,13 @@
 package www.Study.Day05.polymorphismTest;
 
 public class Cat extends Animal{
+    public Cat() {
+    }
+
+    public Cat(int age, String color) {
+        super(age, color);
+    }
+
     @Override
     public void eat(String something) {
         System.out.printf("%d岁的%s的猫咪眯着眼睛侧着头吃%s\n",getAge(),getColor(),something);
--- src/www/Study/Day05/polymorphismTest/Dog.java
+++ src/www/Study/Day05/polymorphismTest/Dog.java
@@ -1,6 +1,13 @@
 package www.Study.Day05.polymorphismTest;
 
 public class Dog extends Animal{
+    public Dog() {
+    }
+
+    public Dog(int age, String color) {
+        super(age, color);
+    }
+
     @Override
     public void eat(String something) {
         System.out.printf("%d岁的%s的狗正在死死的抱住%s猛吃\n",getAge(),getColor(),something);
